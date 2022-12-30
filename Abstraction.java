58d201f4bbe7f2e92cb2dfc01d05503888b0459e
mona7891 <113427770+mona7891@users.noreply.github.com>
2022-12-30 18:07:52 +0530

    Add files via upload

---
+++ Abstraction.java
@@ -0,0 +1,45 @@
+//Abstraction
+//Data hiding is the process of protecting members of class from unintended change whereas,abstraction is hiding the implementation details and showing only importatnt / useful parts to the user.
+//An abstract class must be declared with an abstract keyword
+//It can have abstract and non-abstract method 
+//It cannot be instantited.
+//It can have constructors and static method also 
+//It can have final method which will force the subclass not to change the body of the method.
+
+import java.util.*;
+
+abstract class  Animal  //hid
+{
+	abstract  void walk();  // useing Abstract
+	public void eat()       //useing Non-Abstract
+	{
+		System.out.println("Animal eats");
+	}
+}
+class Horse extends Animal
+{
+	public void walk()
+	{
+	 	System.out.println("Walks on 4 legs ");
+	}
+}
+class Chicken extends Animal
+{
+	public void walk()
+	{
+		System.out.println("Walks on 2 legs ");
+	}
+}
+public class Abstraction
+{
+	public static void main(String args[])
+	{
+		Horse horse = new Horse();
+
+		horse.walk();
+		horse.eat(); // non Abstract 
+		//Animal animal = new Animal(); 
+		// Animal is abstract; cannot be instantiated
+		//animal.walk();
+	}
+}
