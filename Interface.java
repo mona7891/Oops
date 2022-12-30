959f532e96dbde05a986c7cc92a3cd56ec0e80ee
mona7891 <113427770+mona7891@users.noreply.github.com>
2022-12-30 18:09:00 +0530

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,44 @@
+//Interface
+//Interface has no constractor
+//Interface has no Abstraction
+// All the fields in interfaces are public,static and final by default
+// All the method are public & abstract by default
+// A class that implements an interface must implement all the methods declared in the interface.
+// interfaces support the functionality of multiple inheritance. 
+
+import java.util.*;
+	//int eyes = 2 ; --->value are public static & final
+interface Animal
+{
+	void walks();
+	//Animal();  --> constractor
+	//void eat();--> Abstract  
+
+}
+// java does not have multiInhritance 
+// but interface have Inhritance
+
+interface Herbivore
+{
+	
+}
+
+class Horse implement Animal,Herbivore      // Interface 
+                                            // multipleInhritance
+{
+	public void walk()
+	{
+		System.out.println("walks on 4 legs ");
+	}
+
+}
+public class Interface
+{
+	public static void main(String args[])
+		// static --> cls sathi common OR Access
+	{
+		Horse horse = new Horse();
+
+	}
+
+}
