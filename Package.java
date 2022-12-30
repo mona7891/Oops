38b27da63e96278931567238e08fc2b77ec52a3f
mona7891 <113427770+mona7891@users.noreply.github.com>
2022-12-30 18:01:30 +0530

    Add files via upload

---
+++ Package.java
@@ -0,0 +1,64 @@
+// well formate // order
+// Import java.util.* 
+// (utility package)
+// when we write a scanner cls use this package 
+
+import java.util.*;
+import bank;
+
+class Shape  //base cls
+{
+        public void area(); // function
+        {
+                System.out.println("display area");
+        }
+
+}
+class Triangle extends Shape  // sub cls
+{
+        public  void area(int l,int h) // function 
+        {
+                System.out.println(1/2*l*h);
+
+        }
+}
+class EquilateralTringle extends Triangle      // multilevel
+{
+        public void area(int l,int h)
+        {
+                System.out.println(1/2*l*h);
+        }
+
+}
+class Circle extends Shape
+{
+        public void area(int r)
+        {
+                System.out.println((3.14)*r*r);
+        }
+}
+public class Package
+{
+        public static void main(String args[])
+        {
+               bank.Account acc1 = new bank.Account();  //package 
+
+	       acc1.name = "Customer1";              
+
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
